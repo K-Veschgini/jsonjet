7518a99d3df6d54669bd49c6e0c2ca6a3b760423
Kambis Veschgini <k.veschgini@veschgini-consulting.com>
2025-07-12 22:24:16 +0200

    cleanup

--- release/lib/src/index.js
+++
@@ -1,6 +0,0 @@
-export { Operator } from './core/operator.js';
-export { Stream } from './core/stream.js';
-export { ScanOperator } from './operators/scan.js';
-export { Filter } from './operators/filter.js';
-export { Map } from './operators/map.js';
-export { Sorter } from './operators/sorter.js'; 
